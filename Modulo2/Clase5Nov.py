65634b5914df77aa197d5336a69a0090eaef376c
laba <abad.lucas.b@gmail.com>
2022-11-11 19:00:33 +0100

    Clase 11 de Noviembre

--- Modulo2/Clase5Nov.py
+++ Modulo2/Clase5Nov.py
@@ -16,10 +16,11 @@ def ejer2():
 
 
 def ejer3():
-    f = open("./Files/tabla-n.txt", "w+")
+
     n = 11
     while not 0 < n < 11:
         n = int(input("Introduce numero del 1 al 10:\t"))
+    f = open(f"./Files/tabla-{n}.txt", "w+")
     for i in range(10):
         f.write(str(n)+f"* {i+1} = {(i+1)*n}\n")
     f.close()
@@ -34,14 +35,15 @@ def ejer4():
         linea = linea[:-1]
         valores = linea.split(";")
         empleados.append({"id": valores[0], "nombre": valores[1], "apellido": valores[2], "nacimiento": valores[3]})
-    print(empleados)
+    print(*empleados, sep="\n")
+
 
 def ejer5():
     f = open("./Files/primos.txt", "w+")
-    lista=list(range(2,100))
+    lista = list(range(2, 100))
     for n in lista:
         prime = True
-        for i in range(2,n):
+        for i in range(2, n):
             if n % i == 0:
                 prime = False
         if prime:
@@ -54,7 +56,7 @@ def ejer6():
     m = 11
     while not 0 < m < 11:
         m = int(input("Introduzca un numero del 1 al 10:\t"))
-    f = open("./Files/tabla-n.txt")
+    f = open(f"./Files/tabla-n.txt")
     lista = f.readlines()
     print(lista[m-1][:-1])
 
@@ -66,9 +68,9 @@ def ejer7():
     precios = (139.99, 99.99, 109.99, 129.99, 74.99, 109.89, 85.99, 119.80)
     stocks = (43, 1, 0, 32, 105, 12, 50, 6)
 
-    f = open("./Files/ticket.txt","w")
-    for i in range(len(productos)):
-        print(f"{i+1}. {productos[i]}")
+    f = open("./Files/ticket.txt", "w")
+    for i in range(1, len(productos)):
+        print(f"{i}. {productos[i]}")
     cad = input("Introduce número de la zapatilla deseada: (* para salir)  ")
     carrito = []
     while cad != "*":
@@ -89,4 +91,25 @@ def ejer8():
     print(maximo)
     f.close()
 
-ejer7()
+
+def ejer10(tipo):
+    import os
+
+    if not os.path.isfile("./Files/contador.txt"):
+        f = open("./Files/contador.txt", "w")
+        f.write("0")
+        f.close()
+    f = open("./Files/contador.txt", "r")
+    cont = int(f.read())
+    f.close()
+    if tipo.lower() == "inc":
+        cont += 1
+    elif tipo.lower() == "dec":
+        cont -= 1
+    else:
+        print("Cont: ", cont)
+    f = open("./Files/contador.txt", "w")
+    f.write(str(cont))
+    f.close()
+
+
